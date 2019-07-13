50fd6b9f1d0ce827b2d453a8ce7f349770fd147a
Caleb Morrison <cmorrison8191@gmail.com>
2019-07-13 14:47:37 -0700

    Preliminary implementation of copying playlists to admin acount

--- copy_to_user.py
+++ copy_to_user.py
@@ -1,6 +1,7 @@
 import configparser
 
 from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
 
 
 def load_config(configuration_file_path='config.ini'):
@@ -22,11 +23,23 @@ def main():
     account = MyPlexAccount(config.get('DEFAULT', 'username'),
                             config.get('DEFAULT', 'password'))
 
-    plex = account.resource(config.get('DEFAULT', 'server name')).connect()
+    admin_server = account.resource(config.get('DEFAULT',
+                                               'server name')).connect()
 
-    playlist = plex.playlist(config.get('DEFAULT', 'playlist name'))
+    user = account.user(config.get('DEFAULT', 'user'))
 
-    playlist.copyToUser(config.get('DEFAULT', 'user'))
+    # Get the token for the machine.
+    token = user.get_token(admin_server.machineIdentifier)
+
+    # Get the user server. We access the base URL by requesting a URL for a
+    # blank key.
+    user_server = PlexServer(admin_server.url(''), token=token)
+
+    playlist = user_server.playlist(config.get('DEFAULT', 'playlist name'))
+
+    playlist.create(admin_server, playlist.title, playlist.items())
+
+    # playlist.copyToUser(config.get('DEFAULT', 'to user'))
 
 
 if __name__ == '__main__':
